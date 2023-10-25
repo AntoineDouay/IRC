83953bdd55fe7187cab17a0bde6f8d03c6d7528f
Tselmeg Adiyamunkh <tselmeg8070@gmail.com>
2023-10-25 20:48:04 +0200

    Hot fix

--- src/Commands/PING.cpp
+++ src/Commands/PING.cpp
@@ -6,11 +6,11 @@ void	Commands::PING()
     // if (_parameters.size() == 0)
     //     return reply (ERR_NOORIGIN, "need server_name as parameter");
 
-    std::string msg = ": ";
+    std::string msg = ":";
     if (_parameters.size() == 0)
-        msg = _user->getNickname() + " PONG :\r\n";
+        msg += _user->getNickname() + " PONG :\r\n";
     else
-        msg = _user->getNickname() + " PONG :" + _parameters[0] + "\r\n";
+        msg += _user->getNickname() + " PONG :" + _parameters[0] + "\r\n";
 
     send(_user->getFD(), msg.c_str(), msg.size(), 0);
     return;
