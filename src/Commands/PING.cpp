e7ab72ef13ace6a221bf126423cfc1810ac0c4c0
TG <tg@tgs-macbook-air.home>
2023-10-25 19:04:04 +0200

    Pong

--- src/Commands/PING.cpp
+++ src/Commands/PING.cpp
@@ -6,7 +6,8 @@ void	Commands::PING()
 	if (_parameters.size() == 0)
 		return reply (ERR_NOORIGIN, "need server_name as parameter");
 
-	std::string msg("PONG: " + _serv->getName() + "\r\n");
+	// std::string msg("PONG: " + _serv->getName() + "\r\n");
+	std::string msg("PONG: " + _user->getNickname() + "\r\n");
 
 	send(_user->getFD(), msg.c_str(), msg.size(), 0);
 
