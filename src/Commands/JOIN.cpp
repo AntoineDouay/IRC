739b5746fbc5fee76c93958b8a11a205e4785269
AntoineDouay <antoinedouay@orange.fr>
2023-11-01 18:03:10 +0100

    clean quit

--- src/Commands/JOIN.cpp
+++ src/Commands/JOIN.cpp
@@ -29,73 +29,62 @@ void	Commands::JOIN()
 			channelList = getChanList(channelList, _parameters[0], &end, &start);
 			break;
 		}
-		// len = end - start;
-		// std::string tmp = _parameters[0].substr(start, len);
-		// cout << "tmp: " << tmp << endl;
-		// start = end + 1;
-		// end = _parameters[0].find(',', start);
-		// channelList.push_back(tmp);
+	
 	}
 	if (channelList.empty())
 		channelList.push_back(_parameters[0]);
-	cout << "WHILE END\n";
-	for (unsigned long i = 0; i < channelList.size(); i++){
-		cout << channelList[i] << endl;
-	}
-	cout << "FOR END" << endl;
-
-	// std::string str;
-	// if (_parameters[0].find(',') != std::string::npos)
-	// 	str = _parameters[0].substr(0, _parameters[0].find(','));
-	// else
-	// 	str = _parameters[0];
+	// cout << "WHILE END\n";
+	// for (unsigned long i = 0; i < channelList.size(); i++){
+	// 	cout << channelList[i] << endl;
+	// }
+	// cout << "FOR END" << endl;
 
 	vector<string>::iterator str = channelList.begin();
 	for(; str != channelList.end(); str++) {
-	Channel *tmp = _serv->findChannel(*str, _serv->getChannel());
-	string	tmp_key;
-	int errValue = -1;
-	if (_parameters.size() == 0)
-		return reply (ERR_NEEDMOREPARAMS, _command.c_str());
+		Channel *tmp = _serv->findChannel(*str, _serv->getChannel());
+		string	tmp_key;
+		int errValue = -1;
+		if (_parameters.size() == 0)
+			return reply (ERR_NEEDMOREPARAMS, _command.c_str());
 
-  if (_parameters.size() >= 2)
-		tmp_key = _parameters[1];
-	else
-	 	tmp_key = "";
-	// for (std::vector<std::string>::iterator it  = _parameters.begin(); it != _parameters.end(); it++) // TODO only for test
-	// 	std::cout << *it << std::endl;
-	if (tmp) {
-		// try {
-			if (tmp->getInviteRestrictionOn())
-				if (!tmp->isInvited(_user))
-					return reply (ERR_INVITEONLYCHAN, tmp->getName().c_str(), "has invite only restriction");
+		if (_parameters.size() >= 2)
+			tmp_key = _parameters[1];
+		else
+			tmp_key = "";
+		// for (std::vector<std::string>::iterator it  = _parameters.begin(); it != _parameters.end(); it++) // TODO only for test
+		// 	std::cout << *it << std::endl;
+		if (tmp) {
+			// try {
+				if (tmp->getInviteRestrictionOn())
+					if (!tmp->isInvited(_user))
+						return reply (ERR_INVITEONLYCHAN, tmp->getName().c_str(), "has invite only restriction");
 
-			errValue = tmp->addUser(_user[0], _user, tmp_key);
-			switch (errValue)
-			{
-			case 0:
-				reply(ERR_BADCHANNELKEY, (*str).c_str());
-				break;
-			case 1:
-				reply(ERR_CHANNELISFULL, (*str).c_str());
-				break;
-			case 2:
-				cerr << "User already in " << *str << " channel\n";
-				break;
-			default:
-				_user->addChannel(tmp);
-				// cout << "username: " << _user[0].getUsername() << " | nickname: " << _user[0].getNickname() << endl;
-				reply(tmp->getUserList(), true, JOIN_WELCOME, _user->getNickname().c_str(), _user->getUsername().c_str(),
-				  _serv->getName().c_str(), tmp->getName().c_str());
-				break;
-			}		
-		// } catch (exception &e) {
-		// 	reply(ERR_BADCHANNELKEY, _parameters[0].c_str());
-		// 	cout << e.what() << endl;
-		// }
-	} else
-		_serv->createChannel(*str, _user, tmp_key);
-	}
+				errValue = tmp->addUser(_user[0], _user, tmp_key);
+				switch (errValue)
+				{
+				case 0:
+					reply(ERR_BADCHANNELKEY, (*str).c_str());
+					break;
+				case 1:
+					reply(ERR_CHANNELISFULL, (*str).c_str());
+					break;
+				case 2:
+					cerr << "User already in " << *str << " channel\n";
+					break;
+				default:
+					_user->addChannel(tmp);
+					// cout << "username: " << _user[0].getUsername() << " | nickname: " << _user[0].getNickname() << endl;
+					reply(tmp->getUserList(), true, JOIN_WELCOME, _user->getNickname().c_str(), _user->getUsername().c_str(),
+					_serv->getName().c_str(), tmp->getName().c_str());
+					break;
+				}		
+			// } catch (exception &e) {
+			// 	reply(ERR_BADCHANNELKEY, _parameters[0].c_str());
+			// 	cout << e.what() << endl;
+			// }
+		} else
+			_serv->createChannel(*str, _user, tmp_key);
+		}
 	// if (_parameters[0] == _serv->getPassword())
 	// 	_user->setStatus(1);
 }
