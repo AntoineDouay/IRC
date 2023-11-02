c6e102d0c280e380c3d9ba3e5d073dcdf2881741
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-11-02 16:25:52 +0100

    remove useless comment

--- src/Commands/PONG.cpp
+++ src/Commands/PONG.cpp
@@ -16,20 +16,3 @@ void    Commands::PONG()
     send(_user->getFD(), msg.c_str(), msg.size(), 0);
     return;
 }
-
-// #include "../../include/Commands.hpp"
-
-// void	Commands::PONG()
-// {
-//     // // if (_parameters.size() == 0)
-//     // //     return reply (ERR_NOORIGIN, "need server_name as parameter");
-
-//     std::string msg = ":";
-//     if (_parameters.size() == 0)
-//          msg += _user->getNickname() + " PING :\r\n";
-//     else
-//          msg += _user->getNickname() + " PING :" + _parameters[0] + "\r\n";
-
-//     send(_user->getFD(), msg.c_str(), msg.size(), 0);
-//     return;
-// }
